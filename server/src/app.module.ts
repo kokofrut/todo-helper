6ea1ba7ead62e208fd27cc0921ff753033e9881b
Arsenii Nikolenko <arseniynikolenko07@gmail.com>
2023-03-14 22:39:23 +0200

    f1

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -24,7 +24,7 @@ import { User } from './user/user.model';
     }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: 'localhost',
+      host: 'dumbo',
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
